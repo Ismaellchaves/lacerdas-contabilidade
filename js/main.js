9af5e1cfeadb7ccb92a06418cd91c2742c5b1aa1
ismaell-chaves-batista <127359916+Ismaellchaves@users.noreply.github.com>
2025-11-02 17:18:44 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let currentSlide = 0;
-  const slides = document.querySelectorAll('.mySlides');
-  const totalSlides = slides.length;
+let slideIndex = 0;
+showSlides();
 
-  function showSlide() {
-    // Esconde todos os slides
-    slides.forEach(slide => {
-      slide.style.display = 'none';
-    });
-    
-    // Mostra o slide atual
-    slides[currentSlide].style.display = 'block';
-    
-    // Avança para o próximo slide
-    currentSlide = (currentSlide + 1) % totalSlides;
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5500); // Tempo de troca dos sliders
+}
 
-  // Inicia o slideshow
-  showSlide();
-  setInterval(showSlide, 4000);
-});
+
+//Evento de Scroll
+window.onscroll = function () {
+  myScrolll();
+}
+
+function myScrolll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
